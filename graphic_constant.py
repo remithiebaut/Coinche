a8ab15c6c801c4869572b49480ca3cfaf67e33ee
remi thiebaut <rthiebaut.csi@gmail.com>
2019-07-01 12:34:06 +0200

    began to define the constant

--- graphic_constant.py
+++ graphic_constant.py
@@ -5,10 +5,15 @@ Created on Sun Mar 24 17:07:46 2019
 @author: rthie
 """
 import pygame 
-import coinche_constant as const
+import Core.coinche_constant as const
 
-screen_size=(800,800)
-card_size=(int(screen_size[0]/10),int(3/2*(screen_size[0]/10)))   
+resolution=(16*2,9*2) #32*18 cell
+cell_size=(43,43)
+screen_size=(resolution[0]*cell_size[0],resolution[1]*cell_size[1])
+
+
+card_size=(int(2*cell_size[0]),int(3*cell_size[0]))
+
 WHITE=(255,255,255)
 RED=(125,0,0)
 GREEN=(0,125,0)
@@ -18,37 +23,79 @@ PURPLE=(125,0,125)
 YELLOW=(125,125,0)
 CYAN=(0,125,125)
 
-position={}
+#grid definition
+grid=[]
+
+for line in range(resolution[0]):
+  grid.append([])
+  for col in range(resolution[1]):
+    grid[line].append((cell_size[0]*line,cell_size[1]*col)+cell_size)
+
+def surface(x1,y1,x2,y2) :
+  """
+  prends les coordonnées des deux carreaux de la grille et renvoie la surface correspondante
+  """
+  return((grid[x1][y1][0],grid[x1][y1][1],(1+x2-x1)*cell_size[0],(1+y2-y1)*cell_size[1]))
+  
+
+
+#areas definition
+
+area={}
+
+area["points"]=surface(0,0,9,3)
+
+area["middle"]=surface(11,4,23,13)
+
+area["j1"]=surface(8,15,23,17)
+
+area["j2"]=surface(0,8,1,15)
+
+area["j3"]=surface(11,0,18,1)
+
+area["j4"]=surface(30,8,31,15)
+
+
+
+
+
+
+
+area["cards"]={}
 
 for i in range(1,5):
-    position["J"+str(i)]={}
+    area["cards"]["J"+str(i)]={}
     
 for j in range(8):
-        #position["J1"][j]=( (screen_size[1],screen_size[1]-card_size[1]), (card_size[0]*j, 2*card_size[0]+card_size[0]*j) )
-        position["J1"][j]=(card_size[0]+card_size[0]*j,screen_size[1]-card_size[0])
-        position["J2"][j]=(-card_size[0]/2,3*card_size[0]+card_size[0]*j/2)
-        position["J3"][j]=(screen_size[0]-4*card_size[0]-card_size[0]*j/2,-card_size[0]/2)
-        position["J4"][j]=(screen_size[0]-card_size[0],screen_size[1]-4*card_size[0]-card_size[0]*j/2)
+        area["cards"]["J1"][j]=(area["j1"][0]+card_size[0]*j,area["j1"][1],card_size[0],card_size[1])
+        area["cards"]["J2"][j]=(area["j2"][0],area["j2"][1]+card_size[1]*j,card_size[0],card_size[1])
+        area["cards"]["J3"][j]=(area["j3"][0]+card_size[0]*j,area["j3"][1],card_size[0],card_size[1])
+        area["cards"]["J4"][j]=(area["j4"][0],area["j4"][1]+card_size[1]*j,card_size[0],card_size[1])
+        '''
+        (card_size[0]+card_size[0]*j,screen_size[1]-card_size[0])
+        area["cards"]["J2"][j]=(-card_size[0]/2,3*card_size[0]+card_size[0]*j/2)
+        area["cards"]["J3"][j]=(screen_size[0]-4*card_size[0]-card_size[0]*j/2,-card_size[0]/2)
+        area["cards"]["J4"][j]=(screen_size[0]-card_size[0],screen_size[1]-4*card_size[0]-card_size[0]*j/2)
+'''
 
+#picture load
 
-
-
-list_image={}
+list_picture={}
 
 ID ="Dos"   
 image = pygame.image.load('images/{}.jpg'.format(ID))
 image = pygame.transform.scale(image,card_size)
-list_image[ID] = image
+list_picture[ID] = image
 
 ID ="Dos_inverse"   
 image = pygame.image.load('images/{}.jpg'.format(ID))
 image = pygame.transform.scale(image,(card_size[1],card_size[0]))
-list_image[ID] = image
+list_picture[ID] = image
 
 for numero in const.liste_numero :
     for couleur in const.liste_couleur[:4]:
         ID = numero+couleur    
         image = pygame.image.load('images/{}.jpg'.format(ID))
         image = pygame.transform.scale(image,card_size)
-        list_image[ID] = image
+        list_picture[ID] = image
 
---
+++ test.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 26 15:27:19 2019
+
+@author: rthiebaut
+"""
+
+import pygame 
+import graphic_constant as gconst
+import Core.coinche_constant as const
+from GraphicCard import GraphicCard
+
+
+def draw_pos(screen,size,position,color=(255,255,255)):    
+    screen.fill(color,position+size)
+
+
+def afficher_cartes(screen, cartes):
+  """
+  Print the hand of cards for each player
+  """
+
+  for j in range(1,5):
+    if j%2==0:
+      inverse=True
+    else :
+      inverse = False
+    for i in range(len(cartes)):
+      screen.blit(cartes[i].affichage(inverse), gconst.position["J"+str(j)][i]) #rendre transparent
+
+
+def draw_rect(screen,size,position,color=(255,255,255)):    
+    screen.fill(color,position+size)
+    
+
+def test():  
+  cartes=[]
+  i=0
+  """
+  for numero in const.liste_numero :
+    cartes.append(GraphicCard(numero,"carreau", (gconst.position["J1"][i][0],gconst.screen_size[1]-gconst.card_size[1]) ))
+    i+=1
+  """
+  pygame.init() 
+  screen=pygame.display.set_mode(gconst.screen_size)
+  screen.fill(gconst.GREEN)                   
+  while True:
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE: #escape
+            break
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT: #test
+      screen.fill(gconst.YELLOW,(100,100,100,100))
+
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+       #draw_rect(screen,gconst.position["J1"][1][0] ,gconst.position["J1"][1][1])
+        screen.fill(gconst.BLUE,(gconst.card_size[0],gconst.screen_size[1]-gconst.card_size[1],110,110))
+        
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_DOWN:
+       #draw_rect(screen,gconst.position["J1"][1][0] ,gconst.position["J1"][1][1])
+        screen.fill(gconst.BLUE,gconst.grid[31][17])
+        
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT : 
+        draw_rect(screen,gconst.card_size,(0,0))
+        """
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_UP :
+      
+        afficher_cartes(screen, cartes)
+      
+    if pygame.mouse.get_pos()[1]<(gconst.area["cards"]["J1"][1]):
+      for carte in cartes:
+        if pygame.mouse.get_pos()[0]>(carte.position[0]) and pygame.mouse.get_pos()[0]<(carte.position[0]+gconst.card_size[0]):
+          if  event.type == pygame.MOUSEBUTTONDOWN : 
+            
+             carte.jouer(screen)
+             delete=cartes.index(carte)
+             cartes=cartes[:delete]+cartes[delete+1:]
+            """
+             
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_1 :
+        screen.fill(gconst.BLUE,gconst.area["j1"])
+        
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_2 :
+        screen.fill(gconst.BLUE,gconst.area["j2"])
+        
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_3 :
+        screen.fill(gconst.BLUE,gconst.area["j3"])
+        
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_4 :
+        screen.fill(gconst.BLUE,gconst.area["j4"])
+        
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_5 :
+        screen.fill(gconst.BLUE,gconst.area["middle"])
+        
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_6 :
+        screen.fill(gconst.BLUE,gconst.area["points"])
+        
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_7 :
+        screen.fill(gconst.BLUE,gconst.area["test"])
+        
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_9 :
+        screen.fill(gconst.GREEN)
+        
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_KP7 :
+        screen.fill(gconst.YELLOW,gconst.area["cards"]["J1"][7])
+
+    if event.type == pygame.KEYDOWN and event.key == pygame.K_KP0 :
+        screen.fill(gconst.YELLOW,gconst.area["cards"]["J1"][0])
+    pygame.display.flip()
+    
+  pygame.quit()
+
+       
+
+
+test()
